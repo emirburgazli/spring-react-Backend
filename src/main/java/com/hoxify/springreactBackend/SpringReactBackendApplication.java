996d6492956e4cb75c256d4a2159f6497371bf1d
Emir Burgazli <emirhanburgazli@gmail.com>
2023-05-04 00:21:10 +0300

    file added.

---
+++ src/main/java/com/hoxify/springreactBackend/SpringReactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hoxify.springreactBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoxify/springreactBackend/SpringReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoxify.springreactBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
